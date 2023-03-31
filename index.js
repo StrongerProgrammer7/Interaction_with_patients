2fabfeeeaa34aff1a1887f89778e32d936a01495
Abdyukov Zufar <swat55551@gmail.com>
2023-03-31 11:39:58 +0300

    Add connect mysql2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
  const express = require('express');
+ const mysql = require('./routers/connectionMySQL');
+ 
  const path = require('path');
  const bodyParser = require('body-parser');
  const fileUpload = require('express-fileupload');
@@ -15,7 +17,16 @@
  {
     try 
     {
-        app.listen(PORT,() => console.log(`Server has been started on the port ${PORT} and env=${NODE_ENV}` ));    
+        //await mysql.authenticate();
+        await mysql.connect(function(error)
+        {
+            if(error)
+                console.log('Unable to connect to the db: ',error);   
+            else
+                console.log('Connection has been established successfully.'); 
+        });
+
+        app.listen(PORT,() => console.log(`Server has been started on the port ${process.env.PORT} and env=${process.env.NODE_ENV}` ));    
     } catch (error) 
     {
         console.log(error);
